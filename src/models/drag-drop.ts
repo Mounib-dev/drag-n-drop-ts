d2a01f3867412b00c32ed842cb677a7b6bfbf3b6
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-20 21:08:37 +0100

    Refactor: moving to a new architecture, separating code, using exports and namespace and bundle the whole thanks to tsconfig

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,17 @@
+// Drag & Drop Interfaces
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    // Drag Over Handler is useful
+    // for "validating" that we are dropping on a valid place and permit the drop
+    dragOverHandler(event: DragEvent): void;
+    // We handle the drop after permitting it
+    dropHandler(event: DragEvent): void;
+    // Useful to revert the visual update if a user abandons the Drag&Drop
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
