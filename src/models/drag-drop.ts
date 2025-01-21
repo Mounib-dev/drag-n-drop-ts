f0d3116ea7eda731a9ef4f199bacaeb34fd80f65
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-21 20:19:42 +0100

    Moving to ES6 Modules architecture using import/export syntaxe

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,17 +1,16 @@
 // Drag & Drop Interfaces
-namespace App {
-  export interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-    dragEndHandler(event: DragEvent): void;
-  }
 
-  export interface DragTarget {
-    // Drag Over Handler is useful
-    // for "validating" that we are dropping on a valid place and permit the drop
-    dragOverHandler(event: DragEvent): void;
-    // We handle the drop after permitting it
-    dropHandler(event: DragEvent): void;
-    // Useful to revert the visual update if a user abandons the Drag&Drop
-    dragLeaveHandler(event: DragEvent): void;
-  }
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+export interface DragTarget {
+  // Drag Over Handler is useful
+  // for "validating" that we are dropping on a valid place and permit the drop
+  dragOverHandler(event: DragEvent): void;
+  // We handle the drop after permitting it
+  dropHandler(event: DragEvent): void;
+  // Useful to revert the visual update if a user abandons the Drag&Drop
+  dragLeaveHandler(event: DragEvent): void;
 }
